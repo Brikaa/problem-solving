18c7edec5e1e61393bcab24fdeb54cfd99fc6beb
Omar Brikaa <brikaaomar@gmail.com>
2023-10-21 19:12:27 +0300

    Prefix, suffix improvements

--- problem-solving/notes/prefix-suffix-range/main.cpp
+++ problem-solving/notes/prefix-suffix-range/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 
-int arr[] = {1, 2, 5, 1, 8, 9, 33, 22};
+int arr[] = {0, 2, 5, 1, 8, 9, 33, 22};
 const int N = sizeof(arr) / sizeof(N);
 int sum_prefix[N];
 int sum_suffix[N];
@@ -17,7 +17,8 @@ int main()
     sum_prefix[i] = sum_prefix[i - 1] + arr[i];
     min_prefix[i] = std::min(min_prefix[i - 1], arr[i]);
   }
-  std::cout << "Sum and minimum from index till index 5: " << sum_prefix[5] << ", " << min_prefix[5] << '\n';
+  std::cout << "Sum and minimum from index 0 till index 5: " << sum_prefix[5] << ", " << min_prefix[5] << '\n';
   std::cout << "Sum from index 3 to index 5: " << sum_prefix[5] - sum_prefix[3] + 1 << '\n';
+  std::cout << "Sum from index 0 to index 5: " << sum_prefix[5] - sum_prefix[0] + 1 << '\n';
   return 0;
 }
--- problem-solving/practice/ilya/main.cpp
+++ problem-solving/practice/ilya/main.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 
-const int N = 1e5;
+const int N = 2e5 + 5;
 char str[N];
 int prefix[N];
-int main() {
+int main()
+{
   /*
-    ###..###
-    01223345
+    #..###
+    010110
+    001123
+
+    #.
+    00
   */
   std::cin >> str;
   prefix[0] = 0;
-  for (int i = 1; str[i]; ++i) {
+  for (int i = 1; str[i]; ++i)
+  {
     prefix[i] = prefix[i - 1] + (str[i - 1] == str[i]);
   }
   int m;
   std::cin >> m;
-  while (m--) {
+  while (m--)
+  {
+    // needed = p[l + 1] + p[l + 2] +... + p[r]
+    // p[l] = p[0] + p[1] + ... + p[l]
+    // p[r] = p[0] + p[1] + ... + p[l - 1] + p[l] + p[l + 1] + ... + p[r]
+    // p[r] - p[l] = p[l + 1] + p[l + 2] + ... + p[r]
     int l, r;
     std::cin >> l >> r;
     std::cout << prefix[r - 1] - prefix[l - 1] << '\n';
