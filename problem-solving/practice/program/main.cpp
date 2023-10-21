9f5871c3bb2939b0ae8841da2d512818b751b9a6
Omar Brikaa <brikaaomar@gmail.com>
2023-10-21 21:43:04 +0300

    More work on 'program'

--- problem-solving/practice/program/main.cpp
+++ problem-solving/practice/program/main.cpp
@@ -5,6 +5,8 @@ char program[N];
 int sum_prefix[N];
 int max_prefix[N];
 int min_prefix[N];
+int max_suffix[N];
+int min_suffix[N];
 
 int main()
 {
@@ -17,14 +19,13 @@ int main()
     - For second part
       - new_sum_start = sum_prefix[l - 1] + arr[r + 1]
       - sum_start_delta = new_sum_start - sum_prefix[r + 1]
-      - new_max = max_prefix[n - 1] + sum_start_delta
-      - new_min = min_prefix[n - 1] + sum_start_delta
+      - new_max = max_suffix[r + 1] + sum_start_delta
+      - new_min = min_suffix[r + 1] + sum_start_delta
     - number of distinct values = max(max_prefix[l - 1], new_max) - min(min_prefix[l - 1], new_min)
     - Need:
       - sum_prefix
       - max_prefix, min_prefix: on the sum prefix
-
-    - TODO: we need to use a suffix sum for the second part
+      - max_suffix, min_suffix: on the sum prefix
   */
   int t;
   std::cin >> t;
@@ -33,6 +34,7 @@ int main()
     int n, m;
     std::cin >> n >> m;
     std::cin >> program;
+
     sum_prefix[0] = ((program[0] == '+') << 1) - 1;
     max_prefix[0] = sum_prefix[0];
     min_prefix[0] = sum_prefix[0];
@@ -42,14 +44,23 @@ int main()
       max_prefix[i] = std::max(max_prefix[i - 1], sum_prefix[i]);
       min_prefix[i] = std::min(min_prefix[i - 1], sum_prefix[i]);
     }
+
+    max_suffix[n - 1] = sum_prefix[n - 1];
+    min_suffix[n - 1] = sum_prefix[n - 1];
+    for (int i = n - 2; i >= 0; --i)
+    {
+      max_suffix[i] = std::max(max_suffix[i + 1], sum_prefix[i]);
+      min_suffix[i] = std::min(min_suffix[i + 1], sum_prefix[i]);
+    }
+
     while (m--)
     {
       int l, r;
       std::cin >> l >> r;
       int new_sum_start = sum_prefix[l - 1] + ((program[r + 1] == '+') << 1) - 1;
       int sum_start_delta = new_sum_start - sum_prefix[r + 1];
-      int new_max = max_prefix[n - 1] + sum_start_delta;
-      int new_min = min_prefix[n - 1] + sum_start_delta;
+      int new_max = max_suffix[r + 1] + sum_start_delta;
+      int new_min = min_suffix[r + 1] + sum_start_delta;
       std::cout << (std::max(max_prefix[l - 1], new_max) - std::min(min_prefix[l - 1], new_min)) << '\n';
     }
   }
