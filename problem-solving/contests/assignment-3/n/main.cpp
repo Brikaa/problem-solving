d3e3ec5c58954ce4af9f643be013bc86783816f5
Omar Brikaa <brikaaomar@gmail.com>
2023-11-29 19:47:06 +0200

    WIP problem n

--- problem-solving/contests/assignment-3/n/main.cpp
+++ problem-solving/contests/assignment-3/n/main.cpp
@@ -1,34 +1,35 @@
 /*
 Set of integers n
-Number of ordered pairs (xi, yi) that satisfy |x - y| <= |x| and |x + y| >= |y| or |x - y| >= |x| and |x + y| <= |y|
+Number of ordered pairs (xi, yi) that satisfy
+x < y
+|x - y| <= |x| and |x + y| >= |y| and |x| < |y| or
+|x - y| >= |x| and |x + y| <= |y| and |x| > |y| or
+|x - y| >= |y| and |x + y| <= |x| and |x| < |y| or
+|x - y| <= |y| and |x + y| >= |x| and |x| > |y|
 
 If x >= 0, look for x < y <= 2x
-if x < 0, look for y >= ceil(-x / 2) or x < y <= 0
+if x < 0, look for ceil(-x / 2) <= y < -x or x < y <= -2x
 
 
--1 1 satisfies
-|-1 - 1| = 2 >= |-1|
-|-1 + 1| = 0 <= |1|
+if x >= 0, look for
+  x < y <= 2x
+  impossible
+  impossible
+  impossible
 
--5 -1 satisfies
-|-5 - -1| = 4 <= |-5|
-|-5 + -1| = 6 >= |-1|
+if x < 0, look for
+  impossible
+  ceil(-x / 2) <= y < -x
+  -x < y <= -2x
+  x < y <= floor(x / 2)
 
--1 0 satisfies
-|-1 - 0| = 1 <= |-1|
-|-1 + 0| = 1 >= 0
+  ===> ceil(-x / 2) <= y <= -2x or x < y <= floor(x / 2)
 
--5 -4
-|-5 - -4| = 1 <= |-5|
-|-5 + -4| = 9 >= |-4|
+-3 2 5
+^  ^
 
--2 1 satisfies
-|-2 -1| = |-3| >= |-2|
-|-2 + 1| = |-1| <= 1
-
--3 1 does not satisfy
-
-5 11 does not satisfy
+-3 5
+8
 */
 #include <algorithm>
 #include <cstdio>
@@ -55,9 +56,12 @@ int main()
     }
     else
     {
-      satisfying += n - (std::upper_bound(points, points + n, ceil(-x / 2.0) - 1) - points);
       satisfying +=
-          (std::upper_bound(points, points + n, 0) - points) - (std::upper_bound(points, points + n, x) - points);
+          (std::upper_bound(points, points + n, floor(x / 2.0)) - points) -
+          (std::upper_bound(points, points + n, x) - points);
+      satisfying +=
+          (std::upper_bound(points, points + n, -2 * x) - points) -
+          (std::upper_bound(points, points + n, ceil(-x / 2.0) - 1) - points);
     }
   }
   printf("%d\n", satisfying);
--- problem-solving/contests/assignment-3/n/test.py
+++ problem-solving/contests/assignment-3/n/test.py
@@ -1,5 +1,5 @@
-# abs(x - y) <= abs(x) and abs(x + y) >= abs(y) or abs(x - y) >= abs(x) and abs(x + y) <= abs(y)
+# abs(x - y) <= abs(x) and abs(x + y) >= abs(y) or abs(x - y) >= abs(y) and abs(x + y) <= abs(x)
 x = int(input())
 y = int(input())
 
-print(abs(x - y) <= abs(x) and abs(x + y) >= abs(y) or abs(x - y) >= abs(x) and abs(x + y) <= abs(y))
+print(abs(x - y) <= abs(x) and abs(x + y) >= abs(y) or abs(x - y) >= abs(y) and abs(x + y) <= abs(x))
