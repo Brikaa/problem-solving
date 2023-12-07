296de6045c40a6c9a4c387112b1593d8ce28e4dd
Omar Brikaa <brikaaomar@gmail.com>
2023-12-07 13:33:15 +0200

    Assignment 3 e trials

--- problem-solving/contests/assignment-2/e/main.cpp
+++ problem-solving/contests/assignment-2/e/main.cpp
@@ -1,42 +1,36 @@
 /*
- */
+4
+132 2 32 4321
+
+expected
+1
+4
+9
+6
+*/
+
+#include <cstdio>
+#include <unordered_set>
 #include <unordered_map>
 #include <string>
-#include <cstdio>
-#include <vector>
-#include <unordered_set>
 #include <iostream>
 
-std::unordered_map<std::string, int> memo;
 const int N = 5e5 + 5;
-std::string numbers[N];
-int n;
+std::unordered_set<char> numbers[N];
+std::unordered_set<char> all_digits;
+std::unordered_map<char, long long> friends;
 
-int count(char what, std::vector<char> exceptions, int start_index)
+long long nc2(int n)
 {
-  std::string exceptions_str = "";
-  for (auto exception : exceptions)
-    exceptions_str += exception;
-  std::string str = what + "n" + exceptions_str;
-  if (memo.find(str) != memo.end())
-    return memo[str];
-  int count = 0;
-  for (int i = start_index; i < n; ++i)
+  if (n < 2)
+    return 0;
+  long long res = 1;
+  for (int i = 1; i <= 2; ++i)
   {
-    std::unordered_multiset<char> numbers_set;
-    for (auto number : numbers[i])
-      numbers_set.insert(number);
-    bool found_exception = false;
-    for (auto exception : exceptions)
-    {
-      if (numbers_set.find(exception) != numbers_set.end())
-        found_exception = true;
-      break;
-    }
-    if (!found_exception)
-      count += numbers_set.count(what);
+    res *= n - i + 1;
+    res /= i;
   }
-  return count;
+  return res;
 }
 
 int main()
@@ -45,11 +39,45 @@ int main()
   std::cin.tie(0);
   std::cout.tie(0);
   int n;
-  scanf("%d", n);
-  for (int i = 0; i < n; ++i)
-    std::cin >> numbers[i];
+  std::cin >> n;
   for (int i = 0; i < n; ++i)
   {
+    std::string number;
+    std::cin >> number;
+    for (auto c : number)
+    {
+      numbers[i].insert(c);
+      all_digits.insert(c);
+    }
   }
+  for (auto i = all_digits.begin(); i != all_digits.end(); ++i)
+  {
+    auto target_char = *i;
+    std::unordered_map<char, int> exceptions;
+    for (int e = 0; e < n; ++e)
+    {
+      if (numbers[e].find(target_char) != numbers[e].end())
+      {
+        ++friends[target_char];
+        for (auto j = all_digits.begin(); j != i; ++j)
+        {
+          exceptions[*j] += numbers[e].find(*j) != numbers[e].end();
+        }
+      }
+    }
+    fprintf(stderr, "friends[%c] before: %lld\n", target_char, friends[target_char]);
+    friends[target_char] = nc2(friends[target_char]);
+    for (auto j = all_digits.begin(); j != i; ++j)
+    {
+      fprintf(stderr, "i: %c, exceptions[%c]: %d\n", target_char, *j, exceptions[*j]);
+      friends[target_char] -= nc2(exceptions[*j]);
+    }
+    fprintf(stderr, "friends[%c]: %lld\n", target_char, friends[target_char]);
+  }
+
+  long long no_friends = 0;
+  for (auto i : all_digits)
+    no_friends += friends[i];
+  printf("%lld\n", no_friends);
   return 0;
 }
