c3e5ddc0f5e2437783531662d81b2f11f0e4d0aa
Omar Brikaa <brikaaomar@gmail.com>
2023-12-30 20:36:25 +0200

    WIP problem f

--- problem-solving/contests/assignment-5/f/main.cpp
+++ problem-solving/contests/assignment-5/f/main.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <string>
 
-typedef unsigned int ui;
+typedef int ui;
 typedef unsigned long long ull;
 
 const ui N = 1e5 + 5;
@@ -15,9 +15,11 @@ ui current_index;
 bool in_right;
 bool visited[2][N];
 ui n, k;
+ui water_level;
 
 bool increase_current_index(int delta)
 {
+  std::cerr << current_index << ' ' << current_index + delta << '\n';
   current_index += delta;
   if (visited[in_right][current_index])
     return false;
@@ -29,26 +31,50 @@ bool solve()
 {
   if (current_index >= n)
     return true;
+  if (water_level >= current_index)
+    return false;
   std::string &current_wall = in_right ? right_wall : left_wall;
   if (current_wall[current_index] == 'X')
     return false;
+
+  bool solved = false;
+
   while (current_index != n - 1 && current_wall[current_index + 1] != 'X')
+  {
+    in_right = !in_right;
+    if (!increase_current_index(k))
+      return false;
+    ++water_level;
+    solved |= solve();
+    if (solved)
+      return true;
+    in_right = !in_right;
+    increase_current_index(-k);
+    --water_level;
+
     if (!increase_current_index(1))
       return false;
-  bool solved = false;
-  // After I go to the opposite wall, I can jump higher than where I was
-  // const ui original_index = current_index;
+    ++water_level;
+  }
+
   while (~current_index && current_wall[current_index] != 'X')
   {
     in_right = !in_right;
     if (!increase_current_index(k))
       return false;
+    ++water_level;
     solved |= solve();
     if (solved)
-      break;
+      return true;
     in_right = !in_right;
     increase_current_index(-k);
-    increase_current_index(-1);
+    --water_level;
+
+    if (!increase_current_index(-1))
+      return false;
+    ++water_level;
+    if (water_level >= current_index)
+      return false;
   }
   return solved;
 }
@@ -59,6 +85,7 @@ int main()
   std::cin.tie(0);
   std::cout.tie(0);
 
+  water_level = -1;
   std::cin >> n >> k;
   std::cin >> left_wall;
   std::cin >> right_wall;
