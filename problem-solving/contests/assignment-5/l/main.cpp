51caeb3d4e3371ce577c3ba452b712957041d26f
Omar Brikaa <brikaaomar@gmail.com>
2024-01-01 14:55:32 +0200

    WIP l

--- problem-solving/contests/assignment-5/l/main.cpp
+++ problem-solving/contests/assignment-5/l/main.cpp
@@ -5,48 +5,57 @@
 typedef unsigned int ui;
 typedef unsigned long long ull;
 
-ui required;
-ui first_cap;
-ui second_cap;
-std::unordered_map<ui, std::unordered_map<ui, ui>> memo;
-std::unordered_map<ui, std::unordered_map<ui, ui>> visited;
-ui vid;
+ull required;
+ull first_cap;
+ull second_cap;
+std::unordered_map<ull, std::unordered_map<ull, ull>> memo;
+std::unordered_map<ull, std::unordered_map<ull, ull>> visited;
+// vid - 1 = unvisited
+// vid = processing
+// vid + 1 = visited
+ull vid;
 
-ui solve(ui first, ui second, ui steps)
+ull solve(ull first, ull second)
 {
-  fprintf(stderr, "%u %u %u\n", first_cap, second_cap, steps);
   if (visited[first][second] == vid)
+    return 1e9;
+  else if (visited[first][second] == vid + 1)
     return memo[first][second];
   visited[first][second] = vid;
-  ui &ret = memo[first][second];
-  if (first > first_cap || second > second_cap)
-    ret = -1;
-  else if (first == required || second == required)
-    ret = steps;
+  ull &ret = memo[first][second];
+  if (first == required || second == required)
+    ret = 0;
   else
-    ret = std::min(
-        std::min(
-            std::min(
-                solve(first - (second_cap - second), second_cap, steps + 1),
-                solve(first_cap, second - (first_cap - first), steps + 1)),
-            std::min(solve(0, second, steps + 1), solve(first, 0, steps + 1))),
-        std::min(solve(first_cap, second, steps + 1), solve(first, second_cap, steps + 1)));
+  {
+    ull opt1 = first <= first - (second_cap - second) ? 1e9 : 1 + solve(first - (second_cap - second), second_cap);
+    ull opt2 = first == 0 ? 1e9 : 1 + solve(0, second);
+    ull opt3 = first == first_cap ? 1e9 : 1 + solve(first_cap, second);
+    ull min1 = std::min(opt1, std::min(opt2, opt3));
+
+    opt1 = second <= second - (first_cap - first) ? 1e9 : 1 + solve(first_cap, second - (first_cap - first));
+    opt2 = second == 0 ? 1e9 : 1 + solve(first, 0);
+    opt3 = second == second_cap ? 1e9 : 1 + solve(first, second_cap);
+    ull min2 = std::min(opt1, std::min(opt2, opt3));
+
+    ret = std::min(min1, min2);
+  }
+  visited[first][second] = vid + 1;
   return ret;
 }
 
 int main()
 {
-  ui t;
-  scanf("%u", &t);
+  ull t;
+  scanf("%llu", &t);
   while (t--)
   {
-    ++vid;
-    scanf("%u%u%u", &first_cap, &second_cap, &required);
-    ui res = solve(0, 0, 0);
-    if (res > 1e9)
+    vid += 3;
+    scanf("%llu%llu%llu", &first_cap, &second_cap, &required);
+    ull res = solve(0, 0);
+    if (res >= 1e9)
       puts("-1");
     else
-      printf("%u\n", res);
+      printf("%llu\n", res);
   }
   return 0;
 }
