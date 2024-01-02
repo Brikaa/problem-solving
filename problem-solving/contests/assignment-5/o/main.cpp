8d1455c80c0b36913a4064038d9f785e8dc46807
Omar Brikaa <brikaaomar@gmail.com>
2024-01-02 19:42:46 +0200

    WIP problem o

--- problem-solving/contests/assignment-5/o/main.cpp
+++ problem-solving/contests/assignment-5/o/main.cpp
@@ -5,10 +5,12 @@ typedef unsigned long long ull;
 
 const ui ROWS = 7;
 const ui COLS = 8;
+const ui PIPS = 7;
 ui grid[ROWS][COLS];
-ui visited[ROWS][COLS];
+ui visited_cells[ROWS][COLS];
+ui visited_dominoes[PIPS][PIPS];
 ui no_solutions;
-const ui table[7][7] = {
+const ui table[PIPS][PIPS] = {
     {1, 2, 3, 4, 5, 6, 7},
     {2, 8, 9, 10, 11, 12, 13},
     {3, 9, 14, 15, 16, 17, 18},
@@ -23,16 +25,22 @@ void print_grid();
 
 void check_and_solve(ui cr, ui cc, ui nr, ui nc)
 {
-  if (nr > ROWS || nc > COLS || visited[nr][nc] == vid || grid[cr][cc] < grid[nr][nc])
+  if (nr > ROWS || nc > COLS || visited_cells[nr][nc] == vid)
     return;
-  visited[nr][nc] = visited[cr][cc] = vid;
+  ui pip1 = grid[cr][cc];
+  ui pip2 = grid[nr][nc];
+  if (visited_dominoes[pip1][pip2] == vid)
+    return;
+  visited_cells[nr][nc] = visited_cells[cr][cc] = vid;
+  visited_dominoes[pip1][pip2] = visited_dominoes[pip2][pip1] = vid;
   ui old_current_elem = grid[cr][cc];
   ui old_next_elem = grid[nr][nc];
-  grid[nr][nc] = grid[cr][cc] = table[grid[cr][cc]][grid[nr][nc]];
+  grid[nr][nc] = grid[cr][cc] = table[pip1][pip2];
   solve(cr, cc + 1);
   grid[cr][cc] = old_current_elem;
   grid[nr][nc] = old_next_elem;
-  visited[nr][nc] = visited[cr][cc] = 0;
+  visited_cells[nr][nc] = visited_cells[cr][cc] = 0;
+  visited_dominoes[pip1][pip2] = visited_dominoes[pip2][pip1] = 0;
 }
 
 void print_grid()
@@ -45,7 +53,6 @@ void print_grid()
 
 void solve(ui cr, ui cc)
 {
-  fprintf(stderr, "%u %u\n", cr, cc);
   if (cr == ROWS - 1 && cc == COLS)
   {
     ++no_solutions;
@@ -53,7 +60,7 @@ void solve(ui cr, ui cc)
   }
   else if (cc == COLS)
     solve(cr + 1, 0);
-  else if (visited[cr][cc] == vid)
+  else if (visited_cells[cr][cc] == vid)
     solve(cr, cc + 1);
   else
   {
@@ -75,7 +82,7 @@ int main()
       for (ui c = 0; c < COLS; ++c)
         if (scanf("%u", &(grid[r][c])) == EOF)
           return 0;
-    printf("%sLayout #%u:\n\n", first ? "" : "\n\n", vid);
+    printf("%sLayout #%u:\n\n", first ? "" : "\n\n\n", vid);
     first = false;
     print_grid();
     printf("Maps resulting from layout #%u are:\n\n", vid);
