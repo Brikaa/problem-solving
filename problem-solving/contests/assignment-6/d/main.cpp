1fed9f54f9efff0970b389014322fa3ff6decae7
Omar Brikaa <brikaaomar@gmail.com>
2024-01-06 22:33:18 +0200

    Finish problem d

--- problem-solving/contests/assignment-6/d/main.cpp
+++ problem-solving/contests/assignment-6/d/main.cpp
@@ -1,25 +1,28 @@
 #include <cstdio>
-#include <unordered_map>
+#include <cstring>
 
 const long long N = 205;
+const long long D = 25;
 long long n;
 long long arr[N];
 long long d;
-std::unordered_map<long long, long long> memo[N][N];
-std::unordered_map<long long, long long> visited[N][N];
+long long memo[N][N][D];
+long long visited[N][N][D];
 long long vid;
 
-long long solve(long long idx, long long remaining, long long sum)
+long long solve(long long idx, long long remaining, long long mod)
 {
-  if (n - idx < remaining)
-    return 0;
   if (remaining == 0)
-    return sum % d == 0;
-  if (visited[idx][remaining][sum] == vid)
-    return memo[idx][remaining][sum];
-  long long ret = solve(idx + 1, remaining - 1, sum + arr[idx]) + solve(idx + 1, remaining, sum);
-  memo[idx][remaining][sum] = ret;
-  visited[idx][remaining][sum] = vid;
+    return mod == 0;
+  if (idx >= n)
+    return 0;
+  if (visited[idx][remaining][mod] == vid)
+    return memo[idx][remaining][mod];
+  long long m = (mod + arr[idx]) % d;
+  if (m < 0) m += d;
+  long long ret = solve(idx + 1, remaining - 1, m) + solve(idx + 1, remaining, mod);
+  memo[idx][remaining][mod] = ret;
+  visited[idx][remaining][mod] = vid;
   return ret;
 }
 
