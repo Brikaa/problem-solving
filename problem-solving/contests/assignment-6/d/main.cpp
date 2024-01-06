899ea893641e631c1c4a957ad84cc5dbef4c9eca
Omar Brikaa <brikaaomar@gmail.com>
2024-01-06 21:07:42 +0200

    WIP problem d

--- problem-solving/contests/assignment-6/d/main.cpp
+++ problem-solving/contests/assignment-6/d/main.cpp
@@ -1,44 +1,43 @@
 #include <cstdio>
 #include <unordered_map>
 
-typedef int ui;
-typedef unsigned long long ull;
+const long long N = 205;
+long long n;
+long long arr[N];
+long long d;
+std::unordered_map<long long, long long> memo[N][N];
+std::unordered_map<long long, long long> visited[N][N];
+long long vid;
 
-const ui N = 205;
-const ui Q = 15;
-ui n;
-ui arr[N];
-ui d;
-std::unordered_map<ui, std::unordered_map<ui, std::unordered_map<ui, ui>>> memo;
-
-ui solve(ui idx, ui remaining, ui sum)
+long long solve(long long idx, long long remaining, long long sum)
 {
-  if (memo[idx][remaining].find(sum) != memo[idx][remaining].end())
-    return memo[idx][remaining][sum];
+  if (n - idx < remaining)
+    return 0;
   if (remaining == 0)
     return sum % d == 0;
-  if (idx >= n)
-    return 0;
-  ui ret = solve(idx + 1, remaining - 1, sum + arr[idx]) + solve(idx + 1, remaining, sum);
+  if (visited[idx][remaining][sum] == vid)
+    return memo[idx][remaining][sum];
+  long long ret = solve(idx + 1, remaining - 1, sum + arr[idx]) + solve(idx + 1, remaining, sum);
   memo[idx][remaining][sum] = ret;
+  visited[idx][remaining][sum] = vid;
   return ret;
 }
 
 int main()
 {
-  ui q;
-  ui t = 1;
-  while (scanf("%u%u", &n, &q), q != 0 || n != 0)
+  long long q;
+  long long t = 1;
+  while (scanf("%lld%lld", &n, &q), q != 0 || n != 0)
   {
-    for (ui i = 0; i < n; ++i)
-      scanf("%u", arr + i);
-    printf("SET %u:\n", t++);
-    for (ui query = 1; query <= q; ++query)
+    for (long long i = 0; i < n; ++i)
+      scanf("%lld", arr + i);
+    printf("SET %lld:\n", t++);
+    for (long long query = 1; query <= q; ++query)
     {
-      memo.clear();
-      ui m;
-      scanf("%u%u", &d, &m);
-      printf("QUERY %u: %u\n", query, solve(0, m, 0));
+      ++vid;
+      long long m;
+      scanf("%lld%lld", &d, &m);
+      printf("QUERY %lld: %lld\n", query, solve(0, m, 0));
     }
   }
   return 0;
